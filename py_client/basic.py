95d67153e63140d80249859ee4b6deaf8ed0ad0b
Makarontoja2 <wloch.jakub2@gmai.com>
2022-12-13 13:05:25 +0100

    master

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = 'http://127.0.0.1:8000/api'
+endpoint = 'http://127.0.0.1:8000/api/'
 
-get_response = requests.get(endpoint)
+get_response = requests.post(endpoint, json={'title': 'batman','content': 'hello world'})
 print(get_response.json())
